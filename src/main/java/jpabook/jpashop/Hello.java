ee989373c2a61c51a2c5bb8b8fbeb7187297586c
kiipo0623 <61893530+kiipo0623@users.noreply.github.com>
2022-06-02 12:32:56 +0900

    save

--- src/main/java/jpabook/jpashop/Hello.java
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -3,8 +3,14 @@ package jpabook.jpashop;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+
 @Getter
 @Setter
 public class Hello {
     private String data;
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -20,11 +20,15 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
+    @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
+    @OneToOne
+    @JoinColumn(name="delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate;
 
+    @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL] enum타입
 }
